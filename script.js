a8ec4e0a9324bd7bdad79162d03483ed2ce87f9d
Satheesha G R <122616518+satheesha-gr@users.noreply.github.com>
2023-06-18 22:28:12 +0530

    Updated version

--- script.js
+++ script.js
@@ -1,104 +1,94 @@
-let clock = () => {
-    let date = new Date();
-    let hours = date.getHours();
-    let minutes = date.getMinutes();
-    let seconds = date.getSeconds();
-  
-    let period = "AM";
-    if (hours === 0) {
-      hours = 12;
-    } else if (hours >= 12) {
-      hours -= 12;
-      period = "PM";
-    }
-  
-    hours = hours < 10 ? `0${hours}` : hours;
-    minutes = minutes < 10 ? `0${minutes}` : minutes;
-    seconds = seconds < 10 ? `0${seconds}` : seconds;
-  
-    let clockHours = `${hours}`;
-    let clockMinutes = `${minutes}`;
-    let clockSeconds = `${seconds}`;
-    let clockPeriod = `${period}`;
-  
-    document.getElementById("clock-hours").innerHTML = clockHours;
-    document.getElementById("clock-minutes").innerHTML = clockMinutes;
-    document.getElementById("clock-seconds").innerHTML = clockSeconds;
-    document.getElementById("clock-period").innerHTML = clockPeriod;
-    setTimeout(clock, 1000);
+let hh = document.getElementById('clock-hours')
+let mm = document.getElementById('clock-minutes')
+let ss =  document.getElementById('clock-seconds')
+let AMPM = document.querySelector('.timer-period');
 
-    let morningMessage = "Grab Some Healthy Breakfast !!";
-    let afternoonMessage = "Let's Have Some Lunch !!";
-    let eveningMessage = "Stop Yawning, Get Some Tea.. It's Just Evening !!";
-    let nightMessage = "Close Your Eyes And Go To Sleep !!";
+let wakeup = document.querySelector('.morning-greetings-message');
+let message = document.querySelector('.greetings-message');
+
+function theTimer() {
+    let d =new Date();
+    let hours =d.getHours();
+    let minutes = d.getMinutes();
+    let seconds = d.getSeconds();
+    hh.innerHTML = hours;
+    mm.innerHTML = minutes;
+    ss.innerHTML = seconds;
+
+   // To change content below the clock
+    if (hours>=4 && hours<10 ){
+        message.innerHTML = "GRAB SOME HEALTHY BREAKFAST!!!"
+        
+    }
+    else if (hours>=10 && hours<16 ){
+        message.innerHTML = " LET'S HAVE SOME LUNCH !!"
+        
+    }
+    else if (hours>=16 && hours<20 ){
+        message.innerHTML = "STOP YAWNING, GET SOME TEA.. ITS JUST EVENING!"
+    }
+    else{
+        message.innerHTML = "CLOSE YOUR EYES AND GO TO SLEEP"
+    }
     
-    let morningText = "Good Morning!! Wake Up !!";
-    let afternoonText = "Good Afternoon !! Take Some Sleep";
-    let eveningText = "Good Evening !!";
-    let nightText = "Good Night !!";
-
-    // access image in the HTML document
-    let image = document.getElementById("photo");
-
-    if (period === "AM" && 1 <= hours && hours <= 12) {
-        document.getElementById("greeting-message").innerHTML = morningMessage;
-        document.getElementById("greeting-text").innerHTML = morningText;
-        image.src = "./images/Component-30–1.svg";
-    } else if (period === "PM") {
-        if (period === "PM" && (hours === 12 || (1 <= hours && hours <= 3))) {
-            document.getElementById("greeting-message").innerHTML = afternoonMessage;
-            document.getElementById("greeting-text").innerHTML = afternoonText;
-            image.src = "./images/Group 5183.jpg";
-        } else if (period === "PM" && 4 <= hours && hours <= 7) {
-            document.getElementById("greeting-message").innerHTML = eveningMessage;
-            document.getElementById("greeting-text").innerHTML = eveningText;
-            image.src = "./images/lunch_image.jpg";
-        } else {
-            document.getElementById("greeting-message").innerHTML = nightMessage;
-            document.getElementById("greeting-text").innerHTML = nightText;
-            image.src = "./images/Group 5194.jpg"
-        }
+   // to make clock in 12 hrs format
+    if (hh.innerHTML>12){
+        hh.innerHTML = hh.innerHTML - 12;
+        AMPM.innerHTML = "PM"
     }
-};
+    
+}
+setInterval(() =>{
+    theTimer();
+},1000)
 
-clock();
 
-const setAlarm = () => {
-    let selectMorningTime = document.getElementById("select-morning");
-    let selectNoonTime = document.getElementById("select-afternoon");
-    let selectEveningTime = document.getElementById("select-evening");
-    let selectNightTime = document.getElementById("select-night");
-  
-    let wakeUpTime = selectMorningTime.options[selectMorningTime.selectedIndex].value;
-    let lunchTime = selectNoonTime.options[selectNoonTime.selectedIndex].value;
-    let napTime = selectEveningTime.options[selectEveningTime.selectedIndex].value;
-    let nightTime = selectNightTime.options[selectNightTime.selectedIndex].value;
-  
-    selectMorningTime.addEventListener("click", function handleChange(event) {
-        wakeUpTime = selectMorningTime.options[selectMorningTime.selectedIndex].value;
-    });
-  
-    selectNoonTime.addEventListener("click", function handleChange(event) {
-        lunchTime = selectNoonTime.options[selectNoonTime.selectedIndex].value;
-    });
-    selectEveningTime.addEventListener("click", function handleChange(event) {
-        napTime = selectEveningTime.options[selectEveningTime.selectedIndex].value;
-    });
-    selectNightTime.addEventListener("click", function handleChange(event) {
-        nightTime = selectNightTime.options[selectNightTime.selectedIndex].value;
-    });
+but1 = document.querySelector('.btn')
 
-    // Set Alarm 🔔
-    let setAlarm = document.getElementById("set-alarm-btn");
-  
-    const setTimeValues = () => {
-        document.getElementById("set-wakeup-time").innerHTML = wakeUpTime;
-        document.getElementById("set-lunch-time").innerHTML = lunchTime;
-        document.getElementById("set-nap-time").innerHTML = napTime;
-        document.getElementById("set-night-time").innerHTML = nightTime;
-    };
-  
-    setAlarm.onclick = setTimeValues;
-};
-  
-setAlarm();
+but1.addEventListener('click',() =>{
+    let d =new Date();
+    let hours =d.getHours();
+
+    let img = document.querySelector('#photo')
+    
+    let night = document.getElementById('select-night');
+    let ntext = night.options[night.selectedIndex].textContent;
+    let nPrint = document.querySelector('#set-night-time');
+    nPrint.textContent = ntext ;
+
+    let nap = document.getElementById('select-evening');
+    let naptext = nap.options[nap.selectedIndex].textContent;
+    let napPrint = document.querySelector('#set-nap-time');
+    napPrint.textContent = naptext
+
+    let lunch = document.getElementById('select-afternoon');
+    let ltext = lunch.options[lunch.selectedIndex].textContent;
+    let lPrint = document.querySelector('#set-lunch-time');
+    lPrint.textContent = ltext
+
+    let morn = document.getElementById('select-morning');
+    let mtext = morn.options[morn.selectedIndex].textContent;
+    let mPrint = document.querySelector('#set-wakeup-time');
+    mPrint.textContent = mtext
+
+     // to change the text and image based on selected option 
+   
+    if(parseInt(morn.value) === hours){
+        wakeup.innerHTML = "GOOD MORNING!! WAKE UP !!"
+        img.src="./images/Component-30–1.svg"
+    }
+    else if(parseInt(lunch.value) === hours){
+        wakeup.innerHTML = "GOOD AFTERNOON !! TAKE SOME SLEEP"
+        img.src="./images/Group 5183.jpg"
+      
+    }
+    else if(parseInt(nap.value) === hours){
+        wakeup.innerHTML = "GOOD EVENING !!";
+        img.src="./images/lunch_image.jpg";
+       
+    }
+    else if(parseInt(night.value) === hours){
+        wakeup.innerHTML = "GOOD NIGHT !!"
+        img.src="./images/Group 5194.jpg";
+    }
+})
